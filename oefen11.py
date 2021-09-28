573ab8cc17736b7a6518a28fe60006927fbbd106
Maarteb38 <mfreekg@gmail.com>
2021-09-28 16:18:07 +0200

    werkendeforloop1

--- oefen11.py
+++ oefen11.py
@@ -1,9 +1,12 @@
+import array
+
 import pandas as pd
 
-class kerstkaart:
+class Kerstkaart:
     naam = "nvt"
     def feestwensen(_self):
-        print("vrolijke feestdagen!"+ _self.naam)
+        print("vrolijke feestdagen! "+ _self.naam)
+
 
 
 
@@ -13,10 +16,10 @@ data = pd.read_csv('random123.csv')
 #iedereen een vrolijk kersfeest wensen
 naamarray = data['name']
 for x in range(len(naamarray)):
-    q = kerstkaart()
+    q = Kerstkaart()
     q.naam=naamarray[x]
-    q.feestwensen()
-    print(naamarray[x])
-    print("de kerstman weet dat je naam dit aantal letters telt:")
-    print(len(naamarray[x]))
-    print("hallo")
+    mandje = [1 , 'piet']
+    mandje.append(q.feestwensen())
+
+
+print(mandje)
