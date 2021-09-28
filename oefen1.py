7d153c3abf095ff518ec51b007ecce1a98af8f36
Maarteb38 <mfreekg@gmail.com>
2021-09-28 10:37:46 +0200

    nog een keer

--- oefen1.py
+++ oefen1.py
@@ -1,3 +1,16 @@
-zet iets neer in een kladblok 
-2e verandering
-hallo e ssnene
+import pandas as pd
+
+# Data inladen 
+data = pd.read_csv('random123.csv')
+print(data.head(n=10))
+
+# Data schoonmaken
+data.columns = data.columns.str.upper().str.replace('_', '')
+print(data.head())
+
+data = data.set_index('NAME')
+data=data.drop(['#'], axis=1)
+data=data.drop(['GENERATION'], axis=1)
+data=data.drop(['LEGENDARY'], axis=1)
+data['TYPE 2'].fillna(data['TYPE 1'], inplace=True)
+print(data.head(10))
