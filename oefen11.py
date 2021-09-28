89ec713685f81c6ebb9a090b10c77b1274dadf34
Maarteb38 <mfreekg@gmail.com>
2021-09-28 11:51:35 +0200

    nog een keer

--- oefen11.py
+++ oefen11.py
@@ -1,16 +1,14 @@
 import pandas as pd
 
-# Data Inladen
+# Data inladen
 data = pd.read_csv('random123.csv')
 print(data.head(n=10))
 
-# Data Schoonmaken
+# Data schoonmaken
 data.columns = data.columns.str.upper().str.replace('_', '')
 print(data.head())
 
-data = data.set_index('name')
-data=data.drop(['#'], axis=1)
-data=data.drop(['generation'], axis=1)
-data=data.drop(['legendary'], axis=1)
-data['TYPE 2'].fillna(data['TYPE 1'], inplace=True)
+data = data.set_index('NAME')
+data=data.drop(['NAME'], axis=1)
+data=data.drop(['LEGENDARY'], axis=1)
 print(data.head(10))
