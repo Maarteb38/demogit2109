4b66564b1a3278693188e02deba5f0f006b9ee12
Maarteb38 <mfreekg@gmail.com>
2021-10-05 11:53:09 +0200

    nog beter22

---
+++ oefenen2.py
@@ -0,0 +1,10 @@
+class Meegeven:
+    def Iets(a):
+        if a >= 1:
+            a = a * a
+            return print(a)
+        else:
+            return print("helaas weinig")
+
+
+Meegeven.Iets(int(input()))
