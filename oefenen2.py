ccd17197cfc82c74112b0ca4241bcf87fe25489f
Maarteb38 <mfreekg@gmail.com>
2021-10-05 14:34:21 +0200

    nog beter1s

--- oefenen2.py
+++ oefenen2.py
@@ -3,10 +3,11 @@ class Meegeven:
     def Functie(goed, a):
         aantalx = goed.count(a)
         print(aantalx)
-
-print("getallen array 3")
+print("hoelang is het array?")
+length = int(input())
+print("getallen array van", length,"lang")
 getar = []
-for x in range(3):
+for x in range(length):
     getar.append(int(input()))
 print("wat is het zoekgetal?")
 getget = int(input())
