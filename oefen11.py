d4bf9428aa0a4ce4ec03b7ad5fe8dd0b895ae150
Maarteb38 <mfreekg@gmail.com>
2021-10-05 10:13:46 +0200

    het lukt dus

--- oefen11.py
+++ oefen11.py
@@ -8,18 +8,16 @@ class Kerstkaart:
         print("vrolijke feestdagen! "+ _self.naam)
 
 
-
-
 # Data inladen
 data = pd.read_csv('random123.csv')
 
 #iedereen een vrolijk kersfeest wensen
+mandje = []
 naamarray = data['name']
 for x in range(len(naamarray)):
     q = Kerstkaart()
     q.naam=naamarray[x]
-    mandje = [1 , 'piet']
-    mandje.append(q.feestwensen())
-
+    q.feestwensen()
+    mandje.append(q.feestwensen)
 
 print(mandje)
